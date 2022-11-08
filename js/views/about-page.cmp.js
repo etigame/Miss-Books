dfb4293c415da4e1af28e29831c1e09c02aadf84
Eti Gal-Or <etim139@gmail.com>
2022-11-08 17:24:37 +0200

    fix bug

--- js/views/about-page.cmp.js
+++ js/views/about-page.cmp.js
@@ -4,7 +4,7 @@ export default {
     template: `
         <section class="about-page">
             <h1>About Miss Books</h1>
-            <img :src="imgUrl" alt="Miss Books" />
+            <img src="img/miss-books.jpg" alt="Miss Books" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro voluptates blanditiis rem quisquam nisi ad sint id tempora, officiis architecto doloribus labore libero consequatur odit ex! Laudantium ut animi at placeat minima, error accusamus architecto officia quo mollitia debitis necessitatibus ipsa, beatae doloribus aliquam aut. Rerum dolore minima fugiat quam voluptatum magni laudantium quae repudiandae veniam saepe, reprehenderit vel doloribus placeat, incidunt architecto esse consequatur quod eum magnam possimus? Molestias facilis consequatur quisquam. Itaque earum nostrum porro explicabo quisquam eius eveniet officia assumenda quod, aperiam, quibusdam veniam ut quasi quos molestias voluptate mollitia praesentium ullam asperiores? Nulla facilis ab hic.</p>
             <!-- <button @click="emit">Emit test event</button> -->
         </section>
@@ -24,9 +24,5 @@ export default {
         //     eventBus.emit('user-msg', {num: 1234, txt: 'baba'})
         // }
     },
-    computed: {
-        imgUrl() {
-            return '../img/miss-books.jpg'
-        }
-    }
+ 
 }
